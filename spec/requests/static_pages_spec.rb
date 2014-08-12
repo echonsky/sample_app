ad4b119207dc38e84189ea2a9067322a59f69429
Nelson Agamata <nelson_s_agamata@yahoo.com>
2014-08-12 11:06:02 +0800

    Added static contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,4 +33,15 @@ describe "StaticPages" do
       expect(page).to have_title('About Us')
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact Us')
+    end
+  end
 end
